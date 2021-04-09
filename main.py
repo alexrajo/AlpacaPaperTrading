f6a6ffacd19d739ddc248ec29f86f3868f20d2b2
Alexander RJ <alexrj09@hotmail.com>
2021-04-09 23:38:22 +0200

    initialized project

---
+++ main.py
@@ -0,0 +1,16 @@
+import alpaca_trade_api as alpaca
+import threading
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def buy():
+    pass
+
+
+def sell():
+    pass
+
+
+if __name__ == "__main__":
+    pass
